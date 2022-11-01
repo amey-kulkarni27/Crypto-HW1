d839c853aa53bfcea15d9ec8dfcf79cfc6ba7146
amey-kulkarni27 <amey2704@gmail.com>
2022-11-01 04:57:40 +0100

    1.1 done

--- HW1/ex1.py
+++ HW1/ex1.py
@@ -1,31 +1,32 @@
-L = []
-N = len(L)
-
-def enc(K, pt):
-    s = len(K) - 2
+def enc(K, pt, L):
+    N = len(L)
+    s = len(K[0])
     rho, rho_0, P = [None for i in range(s)], [None for i in range(s)], [None for i in range(s)] # Permutation, initial position, and turnover points
     for i in range(s):
-        rho[i], rho_0[i], P[i] = K[i]
-    pi = K[-2]
-    sigma = K[-1]
-    ct = ""
+        # print(len(K[i]))
+        rho[i], rho_0[i], P[i] = K[0][i]
+    pi = K[1]
+    sigma = K[2]
+    ct = []
     for j in range(len(pt)):
+    # for j in range(1):
         mj = pt[j]
         if mj not in L:
-            ct += mj
+            ct.append(mj)
+            continue
         else: # This section is for the rotor movement update
             beta = set()
-            rho_0_old = rho_0
-            rho_0 = (rho_0 + 1) % N
+            rho_0_0_old = rho_0[0]
+            rho_0[0] = (rho_0[0] + 1) % N
             beta.add(0)
-            if rho_0_old in P[0]:
-                rho_1 = (rho_1 + 1) % N
+            if rho_0_0_old in P[0]:
+                rho_0[1] = (rho_0[1] + 1) % N
                 beta.add(1)
             for i in range(1, s-1):
                 if i not in beta and rho_0[i] in P[i]:
                     rho_0[i] = (rho_0[i] + 1) % N
                     rho_0[i+1] = (rho_0[i+1] + 1) % N
-                    beta.add(i)
+                    beta.add(i+1)
         
         x = L.index(mj)
 
@@ -42,23 +43,28 @@ def enc(K, pt):
         x = pi[x]
         
         for i in range(s-1, -1, -1):
-            x = sub_tau(x, rho_0[i])
-            x = rho[i].index(x)
             x = add_tau(x, rho_0[i])
+            x = rho[i].index(x)
+            x = sub_tau(x, rho_0[i])
             
         x = sigma[0][x]
+        # print(x)
 
-        ct += L[x]
+        ct.append(L[x])
 
-        K_prime = []
-        for i in range(s):
-            K_prime.append([rho[i], rho_0[i], P[i]])
-        K_prime.append(pi)
-        K_prime.append(sigma)
+    K_prime = []
+    for i in range(s):
+        K_prime.append([rho[i], rho_0[i], P[i]])
+    K_prime.append(pi)
+    K_prime.append(sigma)
 
-        return K_prime, ct
+    return K_prime, ct
 
 lines = [s for s in open('353055/353055-params.txt') if s[0] != '#' and len(s) > 0 and s.isspace()==False]
 
 for line in lines[:7]:
     exec(line)
+
+K = (Q1a_R, Q1a_pi, Q1a_sigma)
+# print(len(Q1a_R[4]))
+print(enc(K, Q1a_pt, Q1a_L)[1] == Q1a_ct)
